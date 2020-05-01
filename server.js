8cf043bbcf5c9ee56e23404ce2d983b7e0323dcc
raudelf <rawdell@outlook.com>
2020-04-30 21:30:40 -0500

    imported the avatar router to use in postman

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
+const AvatarRouter = require('./endpoints/avatar-router.js');
+
 const server = express();
 
 server.use(express.json());
+server.use('/api/avatar', AvatarRouter);
 
 module.exports = server;
